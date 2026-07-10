33202839e9c31e986685134514ee528dc612ebf9
Jhalak <jhalakchauhan62@gmail.com>
2026-07-11 00:34:06 +0530

    Initial project setup with React and routing

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Explore from "./pages/Explore";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar/Navbar";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <h1>Welcome to LocalNexus</h1>
+        <p>Connecting Local Businesses, Services & Artisans</p>
+      </main>
+    </>
+  );
+}
+
+export default Home;
